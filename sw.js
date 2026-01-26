55399e485e614624abc40f8a8348c295bcb822ed
25468ʳ655 <130685579+dmitr777000@users.noreply.github.com>
2026-01-26 16:53:08 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,3 @@
-const CACHE_NAME = 'my-app-cache-v1';
+self.addEventListener('install', event => console.log("hhhhhhhhhhhh"))
+  // Сразу активируем новый SW (опциональны ===
 
-const PRECACHE_URLS = [
-                  // Главная страница
-  '/index.html'
-        // Опционально: fallback-страница
-];
-
-// === INSTALL: Предзагружаем все необходимые ресурсы ===
-self.addEventListener('install', event => {
-  console.log('Service Worker: установка (precache)');
-
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Предзагрузка ресурсов в кэш:', PRECACHE_URLS);
-        return cache.addAll(PRECACHE_URLS);
-      })
-      .catch(error => {
-        console.error('Ошибка предзагрузки:', error);
-      })
-  );
-
-  // Сразу активируем новый SW (опционально)
-});
