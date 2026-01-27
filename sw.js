c729602bc99b64de1045eb670e818fda3a0fcd12
25468ʳ655 <130685579+dmitr777000@users.noreply.github.com>
2026-01-27 20:52:07 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,8 @@
-let x =1;
-for(let i=0;i<999999999999999;i++){x*=654676;}
+//let x =1;
+//for(let i=0;i<999999999999999;i++){x*=654676;}
+function myfff (){
+  setTimeout(
 
-self.addEventListener('install', event => console.log("hhhhhhhhhhhh"))
+self.addEventListener('install', myfff)
   // Сразу активируем новый SW (опциональны ===
 
