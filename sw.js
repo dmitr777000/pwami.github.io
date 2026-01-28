05f6cee2fbdce9d6962568bf40e232f2fecb52e1
25468ʳ655 <130685579+dmitr777000@users.noreply.github.com>
2026-01-28 21:46:49 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,17 @@
 //let x =1;
 //for(let i=0;i<999999999999999;i++){x*=654676;}
-function myfff (){// Внутри Service Worker
+function myfff (mess){// Внутри Service Worker
 self.clients.matchAll().then(clients => {
   clients.forEach(client => {
     client.postMessage({
       type: 'MESSAGE_TYPE',
-      payload: 'Ваше сообщение'
+      payload: `Ваше ${mess}е`
     });
   });
 });
 }
 
 self.addEventListener('activate', myfff)
-setInterval (myfff, 5000)
+setInterval (myfff, 3000)
   // Сразу активируем новый SW (опциональны ===
 
