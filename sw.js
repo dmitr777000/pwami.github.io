9ed17e42bb67a06a2073bd91a7b6f8663eada252
25468ʳ655 <130685579+dmitr777000@users.noreply.github.com>
2026-02-01 15:50:45 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,17 @@
-//let x =1;
+const urlsToCache = [
+  './'
+ // '/index.html,
+  // добавьте сюда остальные ресурсы, которые хотите кешировать
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open("v1c")
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
 //for(let i=0;i<999999999999999;i++){x*=654676;}
 function myfff (mess){// Внутри Service Worker
 self.clients.matchAll().then(clients => {
