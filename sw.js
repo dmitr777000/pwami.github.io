450515417bf9ecf65422425fe8cb8f3e56085b15
25468ʳ655 <130685579+dmitr777000@users.noreply.github.com>
2026-02-01 22:54:17 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -15,17 +15,7 @@ self.addEventListener('install', event => {
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(cachedResponse => {
-      if (cachedResponse) {
-        return cachedResponse; // всегда возвращать из кеша
-      }
-      // Можно вернуть fallback, если кеша нет
-      // например, return caches.match('/offline.html');
-    })
-  );
-});
+
 
 //for(let i=0;i<999999999999999;i++){x*=654676;}
 function myfff (mess, lexical){// Внутри Service Worker
