51d092e9fe2ead5980c0df5611887d0737a1e7d4
25468ʳ655 <130685579+dmitr777000@users.noreply.github.com>
2026-02-04 13:26:40 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,6 @@
-const urlsToCache = [
-  './'
- // '/index.html,
-  // добавьте сюда остальные ресурсы, которые хотите кешировать
-];
+function myfff (mess, lexical){self.clients.matchAll().then(clients => {  clients.forEach(client => client.postMessage("Ваше е ="+mess+lexical));});}//self.addEventListener('activate', myfff)//setInterval (myfff, 3000)  // Сразу активируем новый SW (опциональны ===
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open("v1c")
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-        myfff(cache, "кеш");
-      })
-     .catch(error =>  myfff(error, "install"))
-  );
-});
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request) 
 
-
-
-//for(let i=0;i<999999999999999;i++){x*=654676;}
-function myfff (mess, lexical){// Внутри Service Worker
-self.clients.matchAll().then(clients => {
-  clients.forEach(client => client.postMessage("Ваше е ="+mess+lexical));
-});
-}
-
-//self.addEventListener('activate', myfff)
-//setInterval (myfff, 3000)
-  // Сразу активируем новый SW (опциональны ===
-
+    }  )
