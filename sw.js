85b3731c60aedd24a407468b4b2b8dd2ba3a2cda
25468ʳ655 <130685579+dmitr777000@users.noreply.github.com>
2026-02-15 00:38:15 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-function myfff (mess, lexical){self.clients.matchAll().then(clients => {  clients.forEach(client => client.postMessage("Ваше е ="+mess+lexical));});}//self.addEventListener('activate', myfff)//setInterval (myfff, 3000)  // Сразу активируем новый SW (опциональны ===
+// function myfff (mess, lexical){self.clients.matchAll().then(clients => {  clients.forEach(client => client.postMessage("Ваше е ="+mess+lexical));});}//self.addEventListener('activate', myfff)//setInterval (myfff, 3000)  // Сразу активируем новый SW (опциональны ===
 
 self.addEventListener('fetch', event => {
   event.respondWith(fetch(event.request) 
